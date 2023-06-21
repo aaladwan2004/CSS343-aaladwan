d1ae2d7df6606b90ec94d9ad589163d78421570c
Abdulaziz <aaladwan2004@gmail.com>
2023-06-21 18:47:10 +0000

    readable

--- HW1/main.cpp
+++ HW1/main.cpp
@@ -3,7 +3,9 @@
 #include <algorithm>
 #include <ctime>
 
-std::string intToString(int number)
+using namespace std;
+
+string intToString(int number)
 {
     if (number == 1)
     {
@@ -17,47 +19,47 @@ std::string intToString(int number)
     {
         return "third";
     }
-    return std::to_string(number) + "th";
+    return to_string(number) + "th";
 }
 
-void printShuffledString(std::string str, int iteration)
+void printShuffledString(string str, int iteration)
 {
-    std::cout << "The " << intToString(iteration) << " shuffled output is:\n";
-    std::cout << "+-----+\n";
+    cout << "The " << intToString(iteration) << " shuffled output is:\n";
+    cout << "+-----+\n";
     for (int i = 0; i < 9; i++)
     {
         if (i % 3 == 0)
         {
-            std::cout << "|";
+            cout << "|";
         }
-        std::cout << str[i] << ' ';
+        cout << str[i] << ' ';
         if ((i + 1) % 3 == 0)
         {
-            std::cout << "|\n";
+            cout << "|\n";
         }
     }
-    std::cout << "+-----+\n\n";
+    cout << "+-----+\n\n";
 }
 
 int main(int argc, char *argv[])
 {
     if (argc != 2)
     {
-        std::cerr << "Usage: ./myhw <number_of_shuffles>\n";
+        cerr << "Usage: ./myhw <number_of_shuffles>\n";
         return 1;
     }
 
     int numberOfShuffles = std::stoi(argv[1]);
 
-    std::cout << "Please input 9 digits to be randomly shuffled:\n";
-    std::string input;
-    std::cin >> input;
+    cout << "Please input 9 digits to be randomly shuffled:\n";
+    string input;
+    cin >> input;
 
-    std::srand(std::time(0));
+    srand(std::time(0));
 
     for (int i = 1; i <= numberOfShuffles; i++)
     {
-        std::random_shuffle(input.begin(), input.end());
+        random_shuffle(input.begin(), input.end());
         printShuffledString(input, i);
     }
 

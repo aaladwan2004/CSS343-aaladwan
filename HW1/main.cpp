9149716dadf18898ef2ae6990a544f34d8db3f80
Abdulaziz <aaladwan2004@gmail.com>
2023-06-21 18:43:05 +0000

    box around nubers

--- HW1/main.cpp
+++ HW1/main.cpp
@@ -6,26 +6,37 @@
 std::string intToString(int number)
 {
     if (number == 1)
+    {
         return "first";
+    }
     if (number == 2)
+    {
         return "second";
+    }
     if (number == 3)
+    {
         return "third";
+    }
     return std::to_string(number) + "th";
 }
 
 void printShuffledString(std::string str, int iteration)
 {
     std::cout << "The " << intToString(iteration) << " shuffled output is:\n";
+    std::cout << "+-----+\n";
     for (int i = 0; i < 9; i++)
     {
+        if (i % 3 == 0)
+        {
+            std::cout << "|";
+        }
         std::cout << str[i] << ' ';
         if ((i + 1) % 3 == 0)
         {
-            std::cout << '\n';
+            std::cout << "|\n";
         }
     }
-    std::cout << '\n';
+    std::cout << "+-----+\n\n";
 }
 
 int main(int argc, char *argv[])

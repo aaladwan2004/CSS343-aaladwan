a26c1fbdbc9853ff9f5f616b951359761ec35774
Abdulaziz <aaladwan2004@gmail.com>
2023-06-21 18:52:37 +0000

    Added comments

--- HW1/main.cpp
+++ HW1/main.cpp
@@ -5,6 +5,7 @@
 
 using namespace std;
 
+// convert nubmer to word for the first three to print 'first' 'second' 'third'
 string intToString(int number)
 {
     if (number == 1)
@@ -22,6 +23,7 @@ string intToString(int number)
     return to_string(number) + "th";
 }
 
+// Function to print the shuffled string in grid format
 void printShuffledString(string str, int iteration)
 {
     cout << "The " << intToString(iteration) << " shuffled output is:\n";
@@ -43,20 +45,24 @@ void printShuffledString(string str, int iteration)
 
 int main(int argc, char *argv[])
 {
+    // Check to see if the number of command-line arguments is right
     if (argc != 2)
     {
         cerr << "Usage: ./myhw <number_of_shuffles>\n";
         return 1;
     }
 
+    // Converts the command-line number to an integer
     int numberOfShuffles = std::stoi(argv[1]);
 
     cout << "Please input 9 digits to be randomly shuffled:\n";
     string input;
     cin >> input;
 
-    srand(std::time(0));
+    // random number generator with current time
+    srand(time(0));
 
+    // does the number of shuffles and prints it
     for (int i = 1; i <= numberOfShuffles; i++)
     {
         random_shuffle(input.begin(), input.end());

869184b4831bc2cec25f1e2adbca20a0ef34a3b6
aaladwan2004 <91643542+aaladwan2004@users.noreply.github.com>
2023-07-06 12:52:10 -0700

    Update 5c.cpp

--- HW2/5c.cpp
+++ HW2/5c.cpp
@@ -1,1 +1,17 @@
+class Solution {
+public:
+    TreeNode* searchBST(TreeNode* root, int val) {
+          if (root->val == val){ //checks if the node we are at is the value
+              return root;
+          }
+          if (root->val > val && root->left != nullptr){ // recursively moves to left node if the value is less than the root 
+              return searchBST(root->left, val);
+          }
+          if (root->val < val && root->right != nullptr){// recursively moves to right node if the value is greater than the root 
+              return searchBST(root->right, val);
+          }
+          return nullptr; //if the node is not found return nullptr
+    }
+};
+
 

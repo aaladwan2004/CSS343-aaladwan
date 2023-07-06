2e162394b0e5233b7674ddbc2cd8a9fb8b141ece
aaladwan2004 <91643542+aaladwan2004@users.noreply.github.com>
2023-07-06 12:56:00 -0700

    Update 5e.cpp

--- HW2/5e.cpp
+++ HW2/5e.cpp
@@ -1,1 +1,15 @@
-
+class Solution {
+public:
+    TreeNode* insertIntoBST(TreeNode* root, int val) {
+        if (root == nullptr){ // if the tree is empty then insert node
+            return new TreeNode(val);
+        }
+        if (root->val < val){ //if the node is greater than the value, recursivley move to the right side of the tree 
+            root->right = insertIntoBST(root->right, val);
+        }
+        if (root->val > val){ //if the node is less than the value, recursivley move to the left side of the tree 
+            root->left = insertIntoBST(root->left, val);
+        }
+        return root;
+    }
+};

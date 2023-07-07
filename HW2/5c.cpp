1463b7e48c25e3260f32b18b39f959078785f5eb
aaladwan2004 <91643542+aaladwan2004@users.noreply.github.com>
2023-07-07 12:25:50 -0700

    Update 5c.cpp

--- HW2/5c.cpp
+++ HW2/5c.cpp
@@ -1,17 +1,40 @@
+//Pre Order
 class Solution {
 public:
-    TreeNode* searchBST(TreeNode* root, int val) {
-          if (root->val == val){ //checks if the node we are at is the value
-              return root;
-          }
-          if (root->val > val && root->left != nullptr){ // recursively moves to left node if the value is less than the root 
-              return searchBST(root->left, val);
-          }
-          if (root->val < val && root->right != nullptr){// recursively moves to right node if the value is greater than the root 
-              return searchBST(root->right, val);
-          }
-          return nullptr; //if the node is not found return nullptr
+    vector<int> PreOrder;
+    vector<int> preorderTraversal(TreeNode* root) {
+        if (root == nullptr) return PreOrder;
+        PreOrder.push_back(root->val);
+        preorderTraversal(root->left);
+        preorderTraversal(root->right);
+        return PreOrder;
+        }
+};
+
+//In Order
+class Solution {
+public:
+    vector <int> inOrder;
+    vector<int> inorderTraversal(TreeNode* root) {
+       if (root == nullptr) return inOrder;
+        inorderTraversal(root->left);
+        inOrder.push_back(root->val);
+        inorderTraversal(root->right);
+        return inOrder; 
     }
 };
 
+// Post Order
+class Solution {
+public:
+    vector <int> PostOrder;
+    vector<int> postorderTraversal(TreeNode* root) {
+        if (root == nullptr) return PostOrder;
+        postorderTraversal(root->left);
+        postorderTraversal(root->right);
+        PostOrder.push_back(root->val);
+        return PostOrder; 
+    }
+};
 
+//the only thing that chagnes is which is visted first, we move the when we vist the root accordingly
